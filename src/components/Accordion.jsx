694ffe83fb7d89421c8fccc8f9d59025a8a88597
abc <you@example.com>
2023-11-06 19:51:36 +0330

    v1

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,10 +1,14 @@
 import AccordionItem from "./AccordionItem";
 
-const Accordion = ({data}) => {
-    console.log(data)
-    return ( <div className="accordion">
-       {data.map((item,i)=><AccordionItem number={i} text={item.text} key={i} title={item.title}/>)}
-    </div> );
-}
- 
+const Accordion = ({ data }) => {
+    console.log(data);
+    return (
+        <div className="accordion">
+            {data.map((item, i) => (
+                <AccordionItem number={i} text={item.text} key={i} title={item.title} />
+            ))}
+        </div>
+    );
+};
+
 export default Accordion;
--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 
-const AccordionItem = ({ text,title,number }) => {
-    const [isOpen,setIsOpen]=useState(false)
-    return <div className={`item ${isOpen ?  "open":''}`} onClick={()=>setIsOpen(!isOpen)}>
-        <span className="number">{number}</span>
-        <span className="title ">{title}</span>
-        <span className="icon " >{isOpen ? "+" : "-"}</span>
-        {isOpen&&<span className="content-box ">{text}</span>}
-      
-    </div>;
+const AccordionItem = ({ text, title, number }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div className={`item ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+            <span className="number">{number}</span>
+            <span className="title ">{title}</span>
+            <span className="icon ">{isOpen ? "+" : "-"}</span>
+            {isOpen && <span className="content-box ">{text}</span>}
+        </div>
+    );
 };
 
 export default AccordionItem;
